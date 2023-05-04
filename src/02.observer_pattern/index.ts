f5ea6b8c3f92e79976e12ab09fbd02db8392ec20
ein-studie <boy.is.playing.rocksteady@gmail.com>
2023-05-05 00:05:29 +0900

    feat: :tada: observerの追加

--- src/02.observer_pattern/index.ts
+++ src/02.observer_pattern/index.ts
@@ -1,11 +1,17 @@
 import { CurrentConditionsDisplay } from "./observers/CurrentConditionsDisplay";
+import { StatisticsDisplay } from "./observers/StatisticsDisplay";
 import { WeatherData } from "./subjects/WeatherData";
 
 function run() {
   const weatherData: WeatherData = new WeatherData();
   const currentDisplay = new CurrentConditionsDisplay(weatherData);
+  const statisticsDisplay = new StatisticsDisplay(weatherData);
 
   weatherData.setMeasurements(80, 65, 30);
+  weatherData.setMeasurements(90, 64, 32);
+  
+  weatherData.removeObserver(statisticsDisplay);
+  weatherData.setMeasurements(75, 20, 33);
 }
 
 run();
