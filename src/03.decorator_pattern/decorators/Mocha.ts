030e6c47dc89b4b19b3af46aff2d72623cc4d13d
ein-studie <boy.is.playing.rocksteady@gmail.com>
2023-05-05 15:27:17 +0900

    feat: :tada: CondimentDecorator 3種追加

---
+++ src/03.decorator_pattern/decorators/Mocha.ts
@@ -0,0 +1,18 @@
+import { CondimentDecorator } from "./CondimentDecorator";
+import { Beverage } from "../beverages/Beverage";
+
+export class Mocha extends CondimentDecorator {
+  constructor(public beverage: Beverage) {
+    super();
+
+    this.beverage = beverage;
+  }
+
+  public getDescription(): string {
+    return this.beverage.getDescription() + '、モカ';
+  }
+
+  public cost(): number {
+    return this.beverage.cost() + 0.20;
+  }
+}
---
+++ src/03.decorator_pattern/decorators/Soy.ts
@@ -0,0 +1,18 @@
+import { CondimentDecorator } from "./CondimentDecorator";
+import { Beverage } from "../beverages/Beverage";
+
+export class Soy extends CondimentDecorator {
+  constructor(public beverage: Beverage) {
+    super();
+
+    this.beverage = beverage;
+  }
+
+  public getDescription(): string {
+    return this.beverage.getDescription() + '、豆乳';
+  }
+
+  public cost(): number {
+    return this.beverage.cost() + 0.15;
+  }
+}
