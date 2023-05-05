c708d2cb998ccebf2e59549f11a1670620174635
ein-studie <boy.is.playing.rocksteady@gmail.com>
2023-05-05 15:55:52 +0900

    refactor: :hammer: 変数定義の変更
    キャメルケースとスネークケースが混在しているが
    分かりやすさを優先

--- src/03.decorator_pattern/index.ts
+++ src/03.decorator_pattern/index.ts
@@ -8,20 +8,20 @@ import { Whip } from "./decorators/Whip";
 function run() {
   console.log('--- 注文開始 ---');
 
-  let beverage = new Espresso();
-  console.log(`${beverage.getDescription()} $${beverage.cost()}`);
+  const espresso = new Espresso();
+  console.log(`${espresso.getDescription()} $${espresso.cost()}`);
   
-  let beverage2 = new DarkRoast();
-  beverage2 = new Mocha(beverage2);
-  beverage2 = new Mocha(beverage2);
-  beverage2 = new Whip(beverage2);
-  console.log(`${beverage2.getDescription()} $${beverage2.cost()}`);
+  const darkRoast = new DarkRoast();
+  const darkRoast_mocha = new Mocha(darkRoast);
+  const darkRoast_mocha_mocha = new Mocha(darkRoast_mocha);
+  const darkRoast_mocha_mocha_whip = new Whip(darkRoast_mocha_mocha);
+  console.log(`${darkRoast_mocha_mocha_whip.getDescription()} $${darkRoast_mocha_mocha_whip.cost()}`);
   
-  let beverage3 = new HouseBlend();
-  beverage3 = new Soy(beverage3);
-  beverage3 = new Mocha(beverage3);
-  beverage3 = new Whip(beverage3);
-  console.log(`${beverage3.getDescription()} $${beverage3.cost()}`);
+  const houseBlend = new HouseBlend();
+  const houseBlend_soy = new Soy(houseBlend);
+  const houseBlend_soy_mocha = new Mocha(houseBlend_soy);
+  const houseBlend_soy_mocha_whip = new Whip(houseBlend_soy_mocha);
+  console.log(`${houseBlend_soy_mocha_whip.getDescription()} $${houseBlend_soy_mocha_whip.cost()}`);
 
   console.log('--- 注文終了 ---');
 }
