0e30f187753a1bad4d86872f81247e3098d6e03b
ein-studie <boy.is.playing.rocksteady@gmail.com>
2023-05-14 23:08:31 +0900

    feat: :tada: 動作確認処理 実装

---
+++ src/06.command_pattern/index.ts
@@ -0,0 +1,43 @@
+import { Light } from "./receivers/Light";
+import { LightOnCommand } from "./commands/LightOnCommand";
+import { LightOffCommand } from "./commands/LightOffCommand";
+import { RemoteControl } from "./invokers/RemoteControl";
+import { Stereo } from "./receivers/Stereo";
+import { StereoOnWithCDCommand } from "./commands/StereoOnWithCDCommand";
+import { StereoOffCommand } from "./commands/StereoOffCommand";
+
+function run() {
+  // インボーカ
+  const remoteControl = new RemoteControl();
+
+  // レシーバ
+  const livingRoomLight = new Light("リビングルーム");
+  const kitchebRoomLight = new Light("キッチン");
+  const stereo = new Stereo("リビングルーム");
+
+  // コマンド
+  const livingRoomLightOn: LightOnCommand = new LightOnCommand(livingRoomLight);
+  const livingRoomLightOff: LightOffCommand = new LightOffCommand(livingRoomLight);
+
+  const kitchebRoomLightOn: LightOnCommand = new LightOnCommand(kitchebRoomLight);
+  const kitchebRoomLightOff: LightOffCommand = new LightOffCommand(kitchebRoomLight);
+
+  const stereoOnWithCD: StereoOnWithCDCommand = new StereoOnWithCDCommand(stereo);
+  const stereoOff: StereoOffCommand = new StereoOffCommand(stereo);
+
+  // 実行
+  remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+  remoteControl.setCommand(1, kitchebRoomLightOn, kitchebRoomLightOff);
+  remoteControl.setCommand(2, stereoOnWithCD, stereoOff);
+
+  console.log(remoteControl.printAllCommands());
+
+  remoteControl.onButtonWasPressed(0);
+  remoteControl.offButtonWasPressed(0);
+  remoteControl.onButtonWasPressed(1);
+  remoteControl.offButtonWasPressed(1);
+  remoteControl.onButtonWasPressed(2);
+  remoteControl.offButtonWasPressed(2);
+}
+
+run();
