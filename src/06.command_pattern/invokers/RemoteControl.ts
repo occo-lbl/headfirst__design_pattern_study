afb1132a55953de449396907263ee81e958971f1
ein-studie <boy.is.playing.rocksteady@gmail.com>
2023-05-14 23:07:34 +0900

    feat: :tada: インボーカ 実装
    - RemoteControl

---
+++ src/06.command_pattern/invokers/RemoteControl.ts
@@ -0,0 +1,43 @@
+import { Command } from "../types/Command";
+import { NoCommand } from "../commands/NoCommand";
+
+export class RemoteControl {
+  private onCommands: Command[] = [];
+  private offCommands: Command[] = [];
+
+  constructor() {
+    const noCommand = new NoCommand();
+
+    for (let i = 0; i < 7; i++) {
+      this.onCommands[i] = noCommand;
+      this.offCommands[i] = noCommand;
+    }
+  }
+
+  public setCommand(slot: number, onCommand: Command, offCommand: Command): void {
+    this.onCommands[slot] = onCommand;
+    this.offCommands[slot] = offCommand;
+  }
+
+  public onButtonWasPressed(slot: number): void {
+    this.onCommands[slot].execute();
+  }
+
+  public offButtonWasPressed(slot: number): void {
+    this.offCommands[slot].execute();
+  }
+
+  public printAllCommands(): string {
+    let result = "\n ---------- リモコン ---------- \n";
+
+    this.onCommands.forEach((v, i) => {
+      const slotNum = `スロット ${i}`;
+      const onCommandName = v.constructor.name;
+      const offCommandName = this.offCommands[i].constructor.name;
+
+      result += `[${slotNum}] ${onCommandName} ${offCommandName}\n`;
+    });
+
+    return result;
+  }
+}
