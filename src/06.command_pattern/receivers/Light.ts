4bc514b7adfd40a2b9eb9e90f5fbfd2d4dbe52f1
ein-studie <boy.is.playing.rocksteady@gmail.com>
2023-05-14 23:04:35 +0900

    feat: :tada: レシーバ 実装
    - Lightクラス
    - Stereoクラス

---
+++ src/06.command_pattern/receivers/Light.ts
@@ -0,0 +1,15 @@
+export class Light {
+  private place: string = "";
+
+  constructor(place: string) {
+    this.place = place;
+  }
+
+  public on(): void {
+    console.log(`${this.place} 照明が付いています`);
+  }
+
+  public off(): void {
+    console.log(`${this.place} 照明が消えています`);
+  }
+}
